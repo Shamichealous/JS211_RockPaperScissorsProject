91e277c845795d61965397c8790bcf2ba51e0bd6
shamichealous <ishamichealous@gmail.com>
2022-09-19 21:18:52 -0500

    first push rps js project using if else

--- main.js
+++ main.js
@@ -14,10 +14,67 @@ const rl = readline.createInterface({
 // the function that will be called by the unit test below
 const rockPaperScissors = (hand1, hand2) => {
 
-  // Write code here
-  // Use the unit test to see what is expected
+  hand1 = hand1.toLowerCase().trim()
+  hand2 = hand2.toLowerCase().trim()
 
+  if(hand1 != "rock" && hand1 !="paper" && hand1 !="scissors"){
+    return "Invalid input in hand1"
+  }
+  if(hand2 != "rock" && hand2 !="paper" && hand2 !="scissors"){
+    return "Invalid input in hand2"
+  }
+
+
+  if(hand1 != "rock" && hand1 != "scissors" && hand1 != "paper")
+  {
+    return "Invalid Input in hand1"
+  }
+  if(hand2 != "rock" && hand2 != "scissors" && hand2 != "paper")
+  {
+    return "Invalid Input in hand2"
+  }
+
+  
+    //equal 
+    //return its a tie
+  if(hand1 === hand2 )
+  {
+    return "It's a tie!";
+  }
+
+    //compare two variables 
+
+    // rock possibilities
+    if(hand1 == "rock" && hand2 == "scissors")
+    {
+      return "Hand one wins!"
+    }
+    else if (hand1 == "rock" && hand2 == "paper")
+    {
+      return "Hand two wins!"
+    }
+  
+    // paper possibilities
+  if(hand1 == "paper" && hand2 == "scissors")
+    {
+      return "Hand two wins!"
+    }
+  else if(hand1 == "paper" && hand2 == "rock")
+    {
+      return "Hand one wins!"
+    }
+    // scissor possiblities 
+  if(hand1 == "scissors" && hand2 == "rock")
+    {
+      return "Hand two wins!"
+    }
+  else if(hand1 == "scissors" && hand2 == "paper")
+  {
+    return "Hand one wins!"
+  }
 }
+
+  // Use the unit test to see what is expected
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
